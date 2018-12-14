65c30be458a32ea937a19541b844ba6659030b57
Izzyan Zulkhafli <izzyan.zulkhafli@terang.asia>
2018-12-14 17:51:36 +0800

    add country module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,7 +13,8 @@ import {DashboardModule} from './dashboard/dashboard.module';
 import {FeedbackModule} from './feedback/feedback.module';
 import {MagicModule} from './magic/magic.module';
 import {MagicService} from '../services/magic.service';
-
+import {CountryModule} from './country/country.module';
+import {CountryService} from '../services/country.service';
 
 @NgModule({
   declarations: [
@@ -30,10 +31,12 @@ import {MagicService} from '../services/magic.service';
     AcademyModule,
     FeedbackModule,
     MagicModule,
+    CountryModule,
   ],
   providers: [
     AcademyService,
     MagicService,
+    CountryService,
   ],
   bootstrap: [AppComponent]
 })
